1016adefd085fead0a1f8db0b9670f36db8775b0
mats <mats.tornberg@gmail.com>
2019-09-20 19:21:11 +0200

    fixed spacings

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include "mqtt.h"
 #include "otaWifi.h"
 
-const char* mqttServer = "10.10.10.1";
-const char* mqttClient = "tmpesp";
-const char* testTopic = "esp/test";
+const char *mqttServer = "10.10.10.1";
+const char *mqttClient = "tmpesp";
+const char *testTopic = "esp/test";
 
 mqttHelper mqtt(mqttServer, 1883);
 OTA_WIFI ota_wifi;
@@ -13,9 +13,10 @@ void setup()
   Serial.begin(9600);
   ota_wifi.setup();
   mqtt.connect(mqttClient);
-  
-  mqtt.subscribe(testTopic, [](byte* data, u_int l){
-    if (cmp(data,"ON",l)) {
+
+  mqtt.subscribe(testTopic, [](byte *data, u_int l) {
+    if (cmp(data, "ON", l))
+    {
       Serial.println("MATCHES");
     }
   });
